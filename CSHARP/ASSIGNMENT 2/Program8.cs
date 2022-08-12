49f86a44a1807a45f4220f71db4978d0877bbc8a
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 16:28:26 +0530

    WAP to print whether the number is even or odd

---
+++ CSHARP/ASSIGNMENT 2/Program8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_odd_programme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, result;
+            Console.WriteLine("enter number");
+            a=Convert.ToInt32(Console.ReadLine());
+            if(a%2==0)
+            {
+                Console.WriteLine("number is even");
+            }
+            else
+            {
+                Console.WriteLine("number is odd");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
