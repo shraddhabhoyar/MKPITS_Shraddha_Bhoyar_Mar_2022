9b2fe78d8f06d884d3b4b47a7663768bbc187a9f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:01:13 +0530

    Create a 2d array of 3 rows and columns

---
+++ CSHARP/ASSIGNMENT 4/Program24.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_rows_3_column
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[,] num = new int[3, 3];
+            for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    Console.WriteLine("enter number");
+                    num[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+               
+            }
+            for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    Console.WriteLine(num[i, j] +"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
