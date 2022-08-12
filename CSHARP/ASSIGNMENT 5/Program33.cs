a8e2e2b798a6862f4b0ab5b7195bcf910401154d
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:42:32 +0530

    Create a class having fields num1,num2 and 4 methods add,sub,mul,div

---
+++ CSHARP/ASSIGNMENT 5/Program33.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_sub_mul_div_num1_num2_assignement
+{
+    class collection
+    {
+        public int num1;
+        public int num2;
+        public int resadd = 0;
+        public int ressub = 0;
+        public int resmul = 0;
+        public int resdiv = 0;
+
+        public void additiondata()
+        {
+            resadd = num1 + num2;
+            Console.WriteLine("addition =" + resadd);
+        }
+
+
+        public void substractiondata()
+        {
+            ressub = num1 - num2;
+            Console.WriteLine("substraction =" + ressub);
+        }
+
+        public void multipictationdata()
+        {
+            resmul = num1 * num2;
+            Console.WriteLine("multipication =" + resmul);
+
+        }
+        public void divisondata()
+        {
+            resdiv = num1 / num2;
+            Console.WriteLine("divison =" + resdiv);
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+           collection cal = new collection();
+
+            Console.WriteLine("Enter num 1");
+            cal.num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter num 2");
+            cal.num2 = Convert.ToInt16(Console.ReadLine());
+
+
+            cal.additiondata();
+            cal.substractiondata();
+            cal.multipictationdata();
+            cal.divisondata();
+
+            Console.ReadLine();
+        }
+    }
+}
