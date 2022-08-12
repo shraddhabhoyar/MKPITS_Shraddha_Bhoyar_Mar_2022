64b2000f941d544b01302555b27747fc38397cbb
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 16:44:08 +0530

    WAP to accept pname,prate,pquantity and display totalamount

---
+++ CSHARP/ASSIGNMENT 2/Program11.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_display_totalamount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rate, qty,result;
+            string name;
+            Console.WriteLine("enter productrate");
+            rate=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter quantity");
+            qty=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            name=Console.ReadLine();
+            result = rate * qty;
+            Console.WriteLine("result="+result);
+            
+        }
+    
+    }
+}
