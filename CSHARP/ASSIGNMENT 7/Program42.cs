4ae5201ceedd7ceb19beaa1f7fa6210838216a61
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 20:20:25 +0530

    Create a base class person having fields create a derived class

---
+++ CSHARP/ASSIGNMENT 7/Program42.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_class_base_person_der_class_empno_salary_desi
+{
+    public class person
+    {
+        public string name;
+        public string address;
+    }
+    public class employee : person
+    {
+        public int empno;
+        public int salary;
+        public string designation;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee em=new employee();
+            em.name = "krishna";
+            em.address = "nagpur";
+            em.empno = 123;
+            em.salary = 25000;
+            em.designation = "manager";
+
+
+
+            Console.WriteLine("name=" + em.name);
+            Console.WriteLine("address=" + em.address);
+            Console.WriteLine("empno=" + em.empno);
+            Console.WriteLine("salary=" + em.salary);
+            Console.WriteLine("designation=" + em.designation);
+
+
+
+
+
+        }
+    }
+}
