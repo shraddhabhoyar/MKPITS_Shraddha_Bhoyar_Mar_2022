76aa2516d669214d4249211a8c082d230e414936
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 16:13:45 +0530

    WAP to display empno,empname,hra,da using switch case

---
+++ CSHARP/ASSIGNMENT 2/Program5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hra_da_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string empname, designation;
+            int bonus = 0;
+            Console.WriteLine("enter empname ");
+            empname = Console.ReadLine();
+            Console.WriteLine("enter designation ");
+            designation = Console.ReadLine();
+            if (designation == "manager") 
+            {
+                bonus = 10000;
+            }
+            else if (designation == "clerk") 
+            {
+                bonus = 5000;
+            }
+            else if (designation == "peon") 
+            {
+                bonus = 2000;
+            }
+            Console.WriteLine("bonus="+bonus);
+        }
+        
+
+    }
+}
