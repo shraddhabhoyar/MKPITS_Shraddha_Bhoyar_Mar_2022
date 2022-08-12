b1174607ceeeb5dfbcffd81448affc2cf03239bb
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:11:17 +0530

    Create a method to accept a number and print the table of that number

---
+++ CSHARP/ASSIGNMENT 4/Program25.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_table_using_method
+{
+    internal class Program
+    {
+        static void Main(int a)
+        {
+            for(int i=0;i<=10;i++)
+            {
+                Console.WriteLine("{0}*{1}={2}",i, a, (i * a) );
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("enter any number");
+            a=Convert.ToInt32(Console.ReadLine());
+            Main(a);
+       }
+    }
+}
