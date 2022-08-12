6397454689b58a0299133d3cd5628b36e12c900f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 16:37:41 +0530

    WAP to display whether it is vowel or not using switch

---
+++ CSHARP/ASSIGNMENT 2/Program9.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vovwels_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("enter char ");
+            ch = Convert.ToChar(Console.ReadLine());
+            switch (ch)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("it is a vowel");
+                    break;
+                
+                default:
+                    Console.WriteLine("it is not a vowel");
+                    break;
+
+                    
+            }
+            Console.ReadLine();
+
+
+        }
+
+    }
+    }
+
