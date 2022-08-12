8247d3cec50723cb2af6e4b978a1ec0c48c9873b
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 17:28:38 +0530

    WAP to print factorial using do while loop

---
+++ CSHARP/ASSIGNMENT 3/Program19.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            int fact=1;
+            Console.WriteLine("enter factorial");
+            a = Convert.ToInt32(Console.ReadLine());
+            int i = a;
+            do
+            {
+                Console.WriteLine("fact=" + fact);
+                fact = fact * i;
+                i--;
+
+            } while (i > 0);
+
+
+
+        }
+    }
+}
