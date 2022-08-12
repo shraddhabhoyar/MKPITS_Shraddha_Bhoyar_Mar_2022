d24a3292ed02e342d0313acfdc919431ba859ce3
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 16:41:19 +0530

    WAP to display whether it is vowel or not using if else

---
+++ CSHARP/ASSIGNMENT 2/Program10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vowels_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("enter any char");
+            ch=Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch=='u')
+            {
+                Console.WriteLine("character is vowel");  
+            }
+            else
+            {
+                Console.WriteLine("character is not a vowel");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
