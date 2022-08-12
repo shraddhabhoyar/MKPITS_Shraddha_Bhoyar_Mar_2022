441dc8487716445b7aa3df8125f5827caca884f9
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:35:11 +0530

    create a class product having fields and display methods getproductdata and displayorder

---
+++ CSHARP/ASSIGNMENT 5/Program31.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_getdata_displayorder
+{
+    class product
+    {
+        int prodid;
+        string prodname;
+        float prodrate;
+        int quantity;
+
+
+        public void productdata()
+        {
+            Console.WriteLine("enter product id");
+            prodid=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter product name");
+            prodname=Console.ReadLine();
+            Console.WriteLine("enter product rate");
+            prodrate=Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("enter quantity");
+            quantity=Convert.ToInt32(Console.ReadLine());
+        }
+         public void displayorder()
+        {
+            Console.WriteLine("prodid" + prodid);
+            Console.WriteLine("prodname" + prodname);
+            Console.WriteLine("prodrate" + prodrate);
+            Console.WriteLine("quantity" + quantity);
+
+        }
+
+
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            product prod=new product();
+            prod.productdata();
+            prod.displayorder();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
