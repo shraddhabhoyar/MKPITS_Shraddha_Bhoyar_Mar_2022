83cf40ddd08a39893116f09552ea4cc6a0a4d45d
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 20:24:48 +0530

    Create a base class person and derived class employee having fields

---
+++ CSHARP/ASSIGNMENT 7/Program43.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_person_employee
+{
+    public class Person
+    {
+        String name;
+        String address;
+        public void getPersonData(String name, String address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        public void displayPersonData()
+        {
+            Console.WriteLine("name : " + name);
+            Console.WriteLine("address : " + address);
+
+        }
+    }
+    public class employee : Person
+    {
+        int empno;
+        int salary;
+        public void getStudentData(int empno, int salary)
+        {
+            this.empno = empno;
+            this.salary = salary;
+        }
+        public void displayStudentData()
+        {
+            Console.WriteLine("empno : " + empno);
+            Console.WriteLine("salary : " + salary);
+
+        }
+    }
+    class TestInheritance
+    {
+        public static void Main(string[] args)
+        {
+           employee p1 = new employee();//creating object of child class\
+            p1.getPersonData("krishna", "nagpur");
+            p1.getStudentData(111, 25000);
+            p1.displayStudentData();
+            p1.displayPersonData();
+        }
+    }
+}
