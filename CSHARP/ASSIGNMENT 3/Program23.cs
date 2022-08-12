8f3ed7d9edaa75a215c746bf235f9f4afa9dc782
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 17:43:29 +0530

    WAP to accept no and print whether it is prime or not using do while

---
+++ CSHARP/ASSIGNMENT 3/Program23.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while_print_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i;
+            int count = 0;
+            Console.WriteLine("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            i = 1;
+            do
+            {
+                if (num % i == 0)
+                {
+
+                    count = count + 1;
+
+
+                }
+                i++;
+
+            } while (i <= num);
+
+                if (count == 2)
+            {
+                Console.WriteLine("prime number");
+
+
+            }
+            else
+            {
+                Console.WriteLine("not a prime number");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
