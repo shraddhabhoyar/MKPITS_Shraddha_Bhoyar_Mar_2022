71346bb77761bc753607e63aacdb5f14864fb5f5
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:37:34 +0530

    Create a class car having fields and display method getcardata and displaycardata

---
+++ CSHARP/ASSIGNMENT 5/Program32.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_get_display_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
