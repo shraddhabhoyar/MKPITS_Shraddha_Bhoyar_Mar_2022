732c120809f9f64ba5d5f7b9378a509e5ea452bd
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 15:45:03 +0530

    Display The Area Of Reactangle

---
+++ CSHARP/ASSIGNMENT 1/Program3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace display_the_area_of_a_rectangle_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("enter length");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter breadth");
+            b = Convert.ToInt32(Console.ReadLine());
+            int result = a * b;
+            Console.WriteLine("area of rectangle=" + result);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
