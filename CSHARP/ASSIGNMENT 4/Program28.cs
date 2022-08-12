aac0e81cf905a565e2db9ef6da786b306661eaa9
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:21:48 +0530

    Create 2 array of 3r 3c and store addition in 3r

---
+++ CSHARP/ASSIGNMENT 4/Program28.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_4_Q_5_addition_of_3_by_3_matrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[,] num1 = new int[3, 3];
+            int[,] num2 = new int[3, 3];
+            int[,] sum = new int[3, 3];
+            Console.WriteLine("Enter Array Element");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    num1[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Enter Array Element");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    num2[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sum[i, j] = num1[i, j] + num2[i, j];
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(sum[i, j] + "\t");
+                }
+
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
