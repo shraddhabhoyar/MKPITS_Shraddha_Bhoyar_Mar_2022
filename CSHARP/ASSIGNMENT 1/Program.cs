344fc3c2fd93dd02548056dae4229951b509b109
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 14:16:18 +0530

    Addition of 3 numbers

---
+++ CSHARP/ASSIGNMENT 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addition_of_three_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("enter number 1");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number 2");
+            b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number 3");
+            c=Convert.ToInt32(Console.ReadLine());
+            int result = a + b + c;
+            Console.WriteLine("addition of 3 numbers="+result);
+            Console.ReadLine();
+        }
+    }
+}
