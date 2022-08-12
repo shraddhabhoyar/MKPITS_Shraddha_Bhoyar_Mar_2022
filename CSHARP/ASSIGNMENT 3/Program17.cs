4035a6d63fe5cf685a92cf8da5619ec532fc7f74
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 17:24:31 +0530

    WAP to ptint factorial using for loop

---
+++ CSHARP/ASSIGNMENT 3/Program17.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loop_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int fact = 1;
+            Console.WriteLine("enter factorial");
+            a = Convert.ToInt32(Console.ReadLine());
+            for(int i = a; i > 0; i--)
+            {
+                fact = fact * i;
+
+            }
+            Console.WriteLine("fact="+fact);
+        }
+    }
+}
