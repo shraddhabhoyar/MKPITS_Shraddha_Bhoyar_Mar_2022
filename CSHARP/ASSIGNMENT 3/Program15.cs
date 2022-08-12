f21585b3c71d2685021fa238f113873919757093
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 17:20:56 +0530

    WAP to print table using while loop

---
+++ CSHARP/ASSIGNMENT 3/Program15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_table_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i=0;
+            int res = 1;
+
+            Console.WriteLine("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+        
+            while (i<= 10)
+            {
+                res = num * i;
+               
+                Console.WriteLine("res="+res);
+                 i++;
+               
+            }
+            
+
+        }
+    }
+}
+
