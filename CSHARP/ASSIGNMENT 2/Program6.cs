3c35dabd2927b689581c71c083ece59f9fdf72e3
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 16:17:42 +0530

    Display empno,empname,hra,da using if else condition

---
+++ CSHARP/ASSIGNMENT 2/Program6.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace da_hra_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace hra_da_basicsalary_programme
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int empno, basic, hra, da, total;
+                string empname;
+                Console.WriteLine("enter employee number");
+                empno = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter employee name");
+                empname = Console.ReadLine();
+                Console.WriteLine("enter basic salary");
+                basic = Convert.ToInt32(Console.ReadLine());
+                hra = basic * 32 / 100;
+                da = basic * 45 / 100;
+                total = basic + hra + da;
+                if
+                Console.WriteLine("hra=" + hra);
+                Console.WriteLine("da=" + da);
+                Console.WriteLine("total=" + total);
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+
+}
+    }
+}
