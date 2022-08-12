4f4ca079275e9cc23865f496005067472134dd1b
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:13:21 +0530

    Create a array to store 5 student name

---
+++ CSHARP/ASSIGNMENT 4/Program26.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_student_name_store_assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //creating a num array of 5 elements
+            string[] name = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter name");
+                name[i] =Console.ReadLine();
+            }
+            //display the values of array
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("name [ {0} ] = {1}", i, name[i]);
+            }
+
+
+        }
+    }
+}
