39621560cf8168455aea786a485c61dc678e0987
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 17:22:41 +0530

    WAP to print tablr using do while

---
+++ CSHARP/ASSIGNMENT 3/Program16.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while_print_table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i = 0;
+            int res = 1;
+
+            Console.WriteLine("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                res = num * i;
+
+                Console.WriteLine("res=" + res);
+                i++;
+
+            } while (i <= 10);
+        }
+    }
+}
+
