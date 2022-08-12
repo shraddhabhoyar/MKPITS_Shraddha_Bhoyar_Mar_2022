6adf4a15eaffa2b64173ff130b73c2b118411b8d
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 19:01:16 +0530

    Create a class product having fields create a constructor with no parameter and method display

---
+++ CSHARP/ASSIGNMENT 6/Program37.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_product_constructor_assignment
+{
+    class product
+    {
+        int productid;
+        string productname;
+        int price;
+        int quantity;
+
+        public product()
+        {
+            productid = 123;
+            productname = "pen";
+            price = 12;
+            quantity = 10;
+
+        }
+        public void display()
+        {
+            Console.WriteLine("productid="+productid);
+            Console.WriteLine("productname="+productname);
+            Console.WriteLine("price="+price);
+            Console.WriteLine("quantity="+quantity);
+         }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            product p1=new product();
+            p1.display();
+
+            Console.ReadLine();
+        }
+    }
+}
