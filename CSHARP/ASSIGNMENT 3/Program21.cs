d09717ea2f2b26d7c6d36c1579e33d69333587b9
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 17:38:16 +0530

    WAP to accept a number and print whether it is prime no or not using for loop

---
+++ CSHARP/ASSIGNMENT 3/Program21.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_or_not_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int count = 0;
+            Console.WriteLine("enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            for(int i= 0; i < num; i++)
+            {
+                if(num%2==0)
+                {
+                    count++;
+                }
+
+            }
+            if(count == 2)
+            {
+                Console.WriteLine("prime number");
+
+            }
+            else
+            {
+                Console.WriteLine("not a prime number");
+            }
+        }
+    }
+}
