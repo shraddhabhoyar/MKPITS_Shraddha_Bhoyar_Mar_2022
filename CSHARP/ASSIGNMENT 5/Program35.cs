53132691cbd0005ef3bde8b467d38a8175bd541d
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:51:04 +0530

    Hra  is 32% of bs,da is 43% of bs,ta is 45% of bs and totalsalary(hra+da+ta+basicsalary)

---
+++ CSHARP/ASSIGNMENT 5/Program35.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hra_da_calculation_assignment
+{
+    class collection
+    {
+        public int empno;
+        public float basicsal, total, HRA, DA, TA;
+        public string empname;
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            collection cal = new collection();
+
+            Console.WriteLine("Enter the employe number");
+            cal.empno = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the basic salary");
+            cal.basicsal = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter the employe name");
+            cal.empname = Console.ReadLine();
+
+            cal.HRA = cal.basicsal * 0.32f;
+            cal.DA = cal.basicsal * 0.45f;
+            cal.TA = cal.basicsal * 0.43f;
+
+            cal.total = cal.basicsal + cal.HRA + cal.DA + cal.TA;
+            Console.WriteLine("name" + cal.empname);
+            Console.WriteLine("id no " + cal.empno);
+            Console.WriteLine("salary" + cal.basicsal);
+            Console.WriteLine("HRA" + cal.HRA);
+            Console.WriteLine("DA" + cal.DA);
+            Console.WriteLine("TA" + cal.TA);
+
+            Console.WriteLine("Total salary of employe" + cal.total);
+            Console.ReadLine();
+        }
+    }
+}
