eb95fa24d82866f4c2cc2d962a10d0b471d35d95
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-12 18:31:29 +0530

    Create a class cust having fields using method getcustomerdata and displaycustomerdata

---
+++ CSHARP/ASSIGNMENT 5/Program30.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cust_getdata_showdata_assignment
+{
+    class customer
+    {
+        int custno;
+        string custname;
+        string custaddress;
+        int custtelno;
+        public void getcustomerdata()
+        {
+            Console.WriteLine("enter customer number");
+            custno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter customer name");
+            custname = Console.ReadLine();
+            Console.WriteLine("enter customer address");
+            custaddress =Console.ReadLine();
+            Console.WriteLine("enter customer telephone number");
+            custtelno = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void displaycustomerdata()
+        {
+            Console.WriteLine("custno=" + custno);
+            Console.WriteLine("custname=" + custname);
+            Console.WriteLine("custaddress=" + custaddress);
+            Console.WriteLine("custtelno=" + custtelno);
+
+
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+            customer cust = new customer();
+            cust.getcustomerdata();
+            cust.displaycustomerdata();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
