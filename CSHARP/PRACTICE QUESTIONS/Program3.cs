7d611df6ba2da7c89ed125d12a49047416abbd55
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-14 22:23:57 +0530

    WAP to find whether given year is leap year or not

---
+++ CSHARP/PRACTICE QUESTIONS/Program3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_to_find_whether_given_year_leap_year_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("enter year");
+            year=Convert.ToInt32(Console.ReadLine());
+            
+            if(((year%4==0) && (year %100!=0))||(year %400==0))
+                    {
+                Console.WriteLine("This is a leap year");
+              }
+            else
+            {
+                Console.WriteLine("This is not a leap year");
+            }
+        }
+    }
+}
