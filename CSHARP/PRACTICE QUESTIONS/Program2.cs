662f80f2bfe7b6c348a33c31081c24f8d9459a6e
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-14 22:04:53 +0530

    WAP to check whether given number is positive or negative

---
+++ CSHARP/PRACTICE QUESTIONS/Program2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_to_check_whether_given_number_is_positive_or_negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            if(num>=0)
+            {
+                Console.WriteLine("number is positive");
+
+            }
+            else
+            {
+                Console.WriteLine("number is negative");
+            }
+        }
+    }
+}
