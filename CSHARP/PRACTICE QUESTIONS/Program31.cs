a17ca2546595b8e9a4b53dc2bfe7eb8060ba7f75
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 21:30:26 +0530

    WAP PATTERN LIKE RIGHT ANGLE TRIANGLE WITH REPEAT NUMBER IN ROW

---
+++ CSHARP/PRACTICE QUESTIONS/Program31.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_repeat_pattern_traingle_angle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+                Console.Write("Input number of rows");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0}", i);
+                Console.Write("\n");
+            }
+        }
+    }
+}
