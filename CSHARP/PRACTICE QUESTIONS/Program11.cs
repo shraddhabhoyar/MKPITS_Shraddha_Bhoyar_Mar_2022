c0c776fb9e9bfeba214824ddeb18c3abca0c1afa
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 17:05:10 +0530

    WAP to read temp and display suitable message according to temp

---
+++ CSHARP/PRACTICE QUESTIONS/Program11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_read_temp_display_suitable_message_acc_temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            Console.WriteLine("enter temp");
+            temp = Convert.ToInt32(Console.ReadLine());
+            if (temp < 0)
+                Console.WriteLine("Freezing whether");
+            else if (temp < 10)
+                Console.Write("Very cold weather");
+            else if (temp < 20)
+                Console.Write("Cold weather");
+            else if (temp < 30)
+                Console.Write("Normal in temp");
+            else if (temp < 40)
+                Console.Write("Its Hot");
+            else
+                Console.Write("Its very hot");
+        }
+    }
+}
