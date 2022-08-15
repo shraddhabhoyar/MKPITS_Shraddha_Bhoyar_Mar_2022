e39be920e1dffd734aa2c2210c2bd629b40e3c94
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 20:10:29 +0530

    WAP to display multiplication table

---
+++ CSHARP/PRACTICE QUESTIONS/Program28.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_display_multiplication_table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, r;
+            Console.Write("enter the number:");
+            r = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= 10; i++)
+            {
+                Console.Write("{0} * {1} = {2} \n", r, i, r * i);
+            }
+        }
+    }
+}
+    
+
