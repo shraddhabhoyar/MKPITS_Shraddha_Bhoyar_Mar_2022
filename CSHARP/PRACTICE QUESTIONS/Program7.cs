1f2ad37b974dd63fff6e2713ffc4b91313f41707
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 12:37:46 +0530

    WAP to find largest number among three

---
+++ CSHARP/PRACTICE QUESTIONS/Program7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_to_find_largest_of_three_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+            Console.WriteLine("enter first number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter third number");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                if (n1 > n3)
+                {
+                    Console.WriteLine("first Number is the greatest among three");
+                }
+                else
+                {
+                    Console.WriteLine("third Number is the greatest among three");
+                }
+            }
+            else if (n2 > n3)
+
+                Console.WriteLine("second Number is the greatest among three");
+            else
+                Console.WriteLine("third Number is the greatest among three");
+            
+        }
+    }
+}
