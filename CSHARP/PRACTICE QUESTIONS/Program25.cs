16b5651d31e3bc36a17447ef23ba4d310a1e4166
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 19:08:17 +0530

    WAP to display n terms natural numbers and their sum

---
+++ CSHARP/PRACTICE QUESTIONS/Program25.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_display_n_terms_natural_numbers_and_their_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+            Console.Write("Input Value of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nThe first {0} natural number are :\n", n);
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}\n ", i);
+                sum += i;
+            }
+            Console.WriteLine("{0}:{1} \n", n, sum);
+        }
+    }
+}
+    
+
+
+
