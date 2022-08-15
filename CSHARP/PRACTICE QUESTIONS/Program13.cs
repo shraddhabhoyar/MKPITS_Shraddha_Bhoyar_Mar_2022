148160f4f1f630d471f198b9a61ab5d59722bb9e
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 17:20:00 +0530

    WAP to check triangle can be formed by the given values for the angles

---
+++ CSHARP/PRACTICE QUESTIONS/Program13.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_check_triangle_value_for_the_angles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anga, angb, angc, sum;   
+            Console.Write("Input angle1 of triangle");
+            anga = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input angle 2 of triangle");
+            angb = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input angle 3 of triangle");
+            angc = Convert.ToInt32(Console.ReadLine());
+
+            sum = anga + angb + angc;
+
+            
+            if (sum == 180)
+            {
+                Console.Write("The triangle is valid");
+            }
+            else
+            {
+                Console.Write("The triangle is not valid");
+            }
+        }
+
+    }
+
+}
+    
