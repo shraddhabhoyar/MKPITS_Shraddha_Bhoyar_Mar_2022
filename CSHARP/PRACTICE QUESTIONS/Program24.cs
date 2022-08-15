041364820323f9fb0461dd427e0ccead1e07d49f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 18:54:20 +0530

    WAP to find sum of 10 natural numbers

---
+++ CSHARP/PRACTICE QUESTIONS/Program24.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_sum_of_10_natural_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sum = 0;
+            Console.Write("The first 10 natural number are:\n");
+            for (i = 1; i <= 10; i++)
+            {
+                sum = sum + i;
+                Console.Write("{0}", i);
+            }
+            Console.Write(" \nSum: {0}\n", sum);
+        }
+    }
+}
+    
+
