f6c1a7ef2f384a553a4c8a0e01aefa998475439e
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 17:11:20 +0530

    WAP to check triangle is equilateral,isosceles or scalene

---
+++ CSHARP/PRACTICE QUESTIONS/Program12.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_check_triangle_equilateral_isosceles_scalene
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sidea, sideb, sidec;
+            Console.Write("Input side 1 of triangle ");
+            sidea = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input side 2 of triangle ");
+            sideb = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input side 3 of triangle");
+            sidec = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (sidea == sideb && sideb == sidec)
+            {
+                Console.Write("This is an equilateral triangle");
+            }
+            else if (sidea == sideb || sidea == sidec || sideb == sidec)
+            {
+                Console.Write("This is an isosceles triangle");
+            }
+            else
+            {
+                Console.Write("This is a scalene triangle");
+            }
+        }
+    }
+
+}
+   
