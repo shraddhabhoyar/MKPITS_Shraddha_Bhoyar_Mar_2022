10473470bf27dc441035f259e694850534422123
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 21:21:44 +0530

    WAP to display pattern ring angle traingle with number

---
+++ CSHARP/PRACTICE QUESTIONS/Program30.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_display_pattern_ring_angle_traingle_with_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+                Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0}", j);
+                Console.Write("\n");
+            }
+        }
+    }
+}
+    
+
