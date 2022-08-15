b57f68bf2a2b10c5da7a43ec2d3d48d8ab97477e
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 21:36:44 +0530

    WAP to make pattern like right angle triangle with number incresed by 1

---
+++ CSHARP/PRACTICE QUESTIONS/Program32.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_pattern_like_right_angle_traingle_with_num_increased_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows, m= 1;
+           Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0} ", m++);
+                Console.Write("\n");
+            }
+        }
+    }
+}
+    
+
