27e4ee0759f7232ef586c107c9513f22acb2d29d
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 21:50:50 +0530

    WAP to display even natural numbers and their sum

---
+++ CSHARP/PRACTICE QUESTIONS/Program34.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_display_even_natural_numbers_and_their_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+            Console.WriteLine("enter number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" even numbers:");
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0} ", 2 * i);
+                sum+= 2 * i;
+            }
+            Console.Write(" {0}: {1} \n", n, sum);
+        }
+    }
+}
+    
+
