8a789deafb7e4546b81885892b96ffca2d019ced
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 21:11:00 +0530

    WAP to display pattern like ring angle traingle using an asterisk

---
+++ CSHARP/PRACTICE QUESTIONS/Program29.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_display_pattern_ring_angle_triangle_asterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+               Console.Write("\n");
+            }
+        }
+    }
+}
+    
+
