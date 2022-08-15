0b09c1d6321dde595488c1d5c0e9e10c0c22e6dd
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 12:21:23 +0530

    WAP to accept height of person and categorized according their height

---
+++ CSHARP/PRACTICE QUESTIONS/Program6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_accept_height_categorize_according_height
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float height;
+            Console.WriteLine("enter the height of person");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            if (height < 150)
+            {
+                Console.WriteLine("the person is dwarf");
+            }
+            else if ((height >= 150) && (height <= 170))
+            {
+                Console.WriteLine("the person is average");
+            }
+            else if ((height >= 170) && (height <= 190))
+            {
+                Console.WriteLine("the person is taller");
+            }
+            else
+            {
+                Console.WriteLine("not accepted");
+            }
+
+             
+        }
+    }
+}
