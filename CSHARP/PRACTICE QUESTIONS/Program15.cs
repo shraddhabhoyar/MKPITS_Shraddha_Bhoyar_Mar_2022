2d73c245c90a9c6bf718f71690c1d92012863f6c
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 17:36:35 +0530

    WAP to calculate profit and loss on a transaction

---
+++ CSHARP/PRACTICE QUESTIONS/Program15.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_to_calculate_profit_loss_on_a_transaction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cp, sp,c;
+
+            Console.Write("Input Cost Price: ");
+            cp = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input Selling Price: ");
+            sp = Convert.ToInt32(Console.ReadLine());
+
+            if (sp > cp)
+            {
+                c = sp - cp;
+                Console.Write(" booked your profit amount : {0}", c);
+            }
+            else if (cp > sp)
+            {
+                c = cp - sp;
+                Console.Write(" got a loss of amount : {0}", c);
+            }
+            else
+            {
+                Console.Write("running in no profit no loss condition");
+            }
+        }
+    }
+
+}
+    
+
