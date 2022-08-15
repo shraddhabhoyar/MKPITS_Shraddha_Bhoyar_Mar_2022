53d7407a170edc644615e7a7cf75054f0bd4fa3f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 17:28:28 +0530

    WAP to check alphabate is a vowel or consonant

---
+++ CSHARP/PRACTICE QUESTIONS/Program14.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_check_alphabate_vowel_consoant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+
+            Console.WriteLine("Enter any character");
+            ch = Convert.ToChar(Console.ReadLine());
+
+
+            
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+
+                Console.WriteLine("this is vowel");
+
+            }
+            else 
+            {
+                Console.WriteLine("this is Consonant");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+    
