67ae6ea8f76cf052045016d694244d267c684d1b
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 21:42:43 +0530

    WAP to calculate factorial

---
+++ CSHARP/PRACTICE QUESTIONS/Program33.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_calculate_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1, num;
+              Console.WriteLine("enter number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+                fact = fact * i;
+
+            Console.WriteLine("{0} : {1}\n", num, fact);
+        }
+    }
+
+}
+   
+
