ad1f90e0fc78a424d58b024b3ed922b49a28f34b
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-15 18:46:54 +0530

    WAP to display first 10 natural numbers

---
+++ CSHARP/PRACTICE QUESTIONS/Program23.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_to_display_first_10_natural_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("The first 10 natural number are:");
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.Write("{0}", i);
+            }
+            
+        }
+    }
+}
+    
+
