c63c10d5b1c4cca65e704d0611d732a5cd6b4fee
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-16 18:28:56 +0530

    WAP to store elements in an array and print it

---
+++ CSHARP/PRACTICE QUESTIONS/Program35.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] array = new int[10];
+            Console.WriteLine("enter elements");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}:", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", array[i]);
+
+            }
+        }
+    }
+}
