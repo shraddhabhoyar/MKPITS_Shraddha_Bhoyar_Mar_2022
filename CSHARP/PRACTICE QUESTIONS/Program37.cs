02326f1d305357f47fc477f72321c3f3e64263cb
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-16 18:49:17 +0530

    WAP to find sum of all elements of the array

---
+++ CSHARP/PRACTICE QUESTIONS/Program37.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAP_to_find_sum_all_elements_of_the_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+            int[] a = new int[50];
+            
+              Console.Write("Input the number of elements to be stored in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                sum += a[i];
+            }
+
+            Console.Write("Sum of all elements stored in the array is : {0}\n", sum);
+        }
+    }
+}
+    
+
