664abae7af0205628f2cb646e072b1d4dfc828a7
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 17:56:00 +0530

    Interface example

---
+++ CSHARP/C SHARP PRACTICE/Program16.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_example
+{
+    interface shape
+    {
+        void draw();
+       
+    }
+    class rectangle : shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("rectangle class");
+        }
+    }
+    class circle : shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("circle class");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            shape s=new rectangle();
+            s.draw();
+            s=new circle();
+            s.draw();
+        }
+    }
+}
