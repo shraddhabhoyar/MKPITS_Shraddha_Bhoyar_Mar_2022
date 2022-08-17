0df810b972cfeebaea36fa4c9001890cb522e285
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 12:58:29 +0530

    Base keyword is used to access fields,const and method of base class

---
+++ CSHARP/C SHARP PRACTICE/Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base_keyword_csharp_which_access_field_base_class
+{
+
+    namespace ConsoleApp19
+    {
+        class person
+        {
+            public string name = "mkpits";
+        }
+        class student : person
+        {
+            public string name = "roshan";
+            public void display()
+            {
+                Console.WriteLine("name = " + name); //roshan
+                Console.WriteLine(" base class field name = " + base.name);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                student s = new student();
+                s.display();
+            }
+        }
+    }
+}
