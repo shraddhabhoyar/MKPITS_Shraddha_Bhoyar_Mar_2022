d8b5a6cff7d460c6bd2b2f8b96604e1b70a68f19
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 13:04:27 +0530

    Csharp base keyword which calls the method of base class animal example

---
+++ CSHARP/C SHARP PRACTICE/Program3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base_keyword_which_calls_method_base_class_animal_example
+{
+    public class animal
+
+    {
+        public void eat()
+        {
+            Console.WriteLine("eating");
+        }
+    }
+
+    public class dog : animal
+    {
+        public void eat()
+        {
+            base.eat();
+            Console.WriteLine("eating bread");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dog g=new dog();
+            g.eat();
+        }
+    }
+}
