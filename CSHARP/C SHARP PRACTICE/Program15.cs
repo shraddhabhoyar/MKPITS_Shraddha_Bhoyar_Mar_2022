21895c0e9203be492af0b19d17d9994bb1937d6e
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 17:44:51 +0530

    Encapsulation example with student id name and email id

---
+++ CSHARP/C SHARP PRACTICE/Program15.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_example_id_name_email
+{
+    class student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string mailid { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            s.id = 1243;
+            s.name = "ankita";
+            s.mailid = "abc@gmail.com";
+
+            Console.WriteLine("id:" + s.id);
+            Console.WriteLine("name:" + s.name);
+            Console.WriteLine("mailid:" + s.mailid);
+
+
+        }
+    }
+}
