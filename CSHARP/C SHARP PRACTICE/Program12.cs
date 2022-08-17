19b9f006836ffaddfd41e70605f6d91716cbdd7c
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 15:00:21 +0530

    Abstract class having abstract method with derived classes

---
+++ CSHARP/C SHARP PRACTICE/Program12.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class_having_abstract_method_with_derived_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
