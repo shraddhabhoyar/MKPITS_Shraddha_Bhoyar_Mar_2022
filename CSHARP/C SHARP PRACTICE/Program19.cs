2ed5e977a4a99a902bea9bcc84aa11e412553d3b
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 18:32:37 +0530

    Interface example four deposite withdraw method

---
+++ CSHARP/C SHARP PRACTICE/Program19.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_example_4_deposite_withdraw_method
+{
+    interface bank
+    {
+        String deposit(int actno, int amt);
+        String withdraw(int actno, int amt);
+    }
+    class account
+    {
+        public int actno;
+        public int bal = 1000;
+        public String showbalance()
+        {
+            return "bal is " + bal;
+        }
+    }
+    class saving : account, bank
+    {
+
+        public String deposit(int actno, int amt)
+        {
+            this.actno = actno;
+            bal = bal + amt;
+            return "deposited successfully successfully ,bal is " + bal;
+        }
+        public String withdraw(int actno, int amt)
+        {
+            this.actno = actno;
+            bal = bal - amt;
+            return "withdraw successfully ,bal is " + bal;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            saving b;
+            b = new saving();
+            String str = b.deposit(1, 200);
+            Console.WriteLine(str);
+
+            str = b.withdraw(1, 100);
+            Console.WriteLine(str);
+            Console.WriteLine(b.showbalance());
+        }
+    }
+}
