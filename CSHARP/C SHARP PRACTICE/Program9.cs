73bc2025845c9868fcc83887f8f051f29f2aa30f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 13:52:23 +0530

    Abstract method example

---
+++ CSHARP/C SHARP PRACTICE/Program9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_method_example
+{
+    abstract class account
+    {
+        public abstract void deposite();
+    }
+    class saving : account
+    {
+        public override void deposite()
+        {
+            Console.WriteLine("hello from saving account");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            account act=new saving();
+            act.deposite();
+        }
+    }
+}
