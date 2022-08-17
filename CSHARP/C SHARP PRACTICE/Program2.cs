c50556be1b79470793aecee703666d301310f4df
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 13:01:15 +0530

    Csharp base keyword which calls the method of base class

---
+++ CSHARP/C SHARP PRACTICE/Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_base_keyword_example_calling_base_class_method
+{
+    class person
+    {
+        public string name = "MKPITS";
+
+        public void display()
+        {
+            Console.WriteLine("base class field name=" + name);
+        }
+    }
+    class student :person
+    {
+        public string name = "SOLUTIONS";
+
+        public void display()
+        {
+            base.display();
+            Console.WriteLine("child class field name=" + name);
+        }
+    }
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s=new student();
+            s.display();
+        }
+    }
+}
