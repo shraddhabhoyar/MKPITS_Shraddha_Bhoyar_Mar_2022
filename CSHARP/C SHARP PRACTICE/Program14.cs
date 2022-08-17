0e969dfe44f0738e5496f7abe972c8a5b925dc8f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-17 17:33:32 +0530

    Encapsulation_example

---
+++ CSHARP/C SHARP PRACTICE/Program14.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_example
+{
+    class employee
+    {
+        public string empname { get;set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee emp=new employee();
+            emp.empname = "ankita";
+            Console.WriteLine("empname:"+emp.empname);
+
+        }
+    }
+}
