d3aa69760897041d8d4abd23f3d481ad8659154f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-18 13:46:22 +0530

    Csharp sealed method example

---
+++ CSHARP/C SHARP PRACTICE/Program21.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_sealed_method
+{
+    class person
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("hello from class");
+        }
+    }
+    class student : person
+    {
+        public string name;
+        public sealed override void display()
+        {
+            Console.WriteLine("hello from department");
+        }
+    }
+    class parttime : student
+    {
+       
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parttime p = new parttime();
+            p.display();
+        }
+    }
+}
+
