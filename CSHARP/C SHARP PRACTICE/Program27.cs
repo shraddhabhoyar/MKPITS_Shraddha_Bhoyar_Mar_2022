8e5254139c1c8b7d6b526beec5ac8068b820de36
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-18 15:28:42 +0530

    Csharp static constructor example

---
+++ CSHARP/C SHARP PRACTICE/Program27.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_static_constructor_example
+{
+    public class account
+    {
+        public string name;
+        public int id;
+        public static float rateofinterest;
+
+        public account(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        static account()
+        {
+            Console.WriteLine("static constructor");
+            rateofinterest = 9.5f;
+        }
+        public void display()
+        {
+            Console.WriteLine(id+""+name+""+rateofinterest);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            account a1 = new account(111, "ankit");
+            account a2 = new account(112, "suraj");
+            a1.display();
+            a2.display();
+        }
+    }
+}
