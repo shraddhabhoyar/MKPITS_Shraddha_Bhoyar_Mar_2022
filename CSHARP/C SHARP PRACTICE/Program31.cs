e9386f4724352064be5b6838bd5045ce81e03e12
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-18 16:35:00 +0530

    Csharp namespace with nested namespace example

---
+++ CSHARP/C SHARP PRACTICE/Program31.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using first;
+using first.second;
+namespace first
+{
+    public class hello
+    {
+        public void sayhello() 
+        { 
+            Console.WriteLine("hello first namespace");
+        }
+    }
+    
+    namespace second
+    {
+        public class welcome
+        {
+            public void sayhello()
+            {
+                Console.WriteLine("hello second namespace");
+            }
+        }
+    }
+}
+ internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            hello h1 = new hello();
+            h1.sayhello();
+            welcome w = new welcome();
+            w.sayhello();
+        }
+    }
+
