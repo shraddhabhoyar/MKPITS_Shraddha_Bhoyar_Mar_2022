d4684f2ec56dbf8f3dd58ad6dc4b35fd5afd1ba6
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-18 13:59:42 +0530

    Csharp static example

---
+++ CSHARP/C SHARP PRACTICE/Program22.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_static
+{
+    class student
+    {
+        public static void display()
+        {
+            Console.WriteLine("hello");
+        }
+        public void display2()
+        {
+            Console.WriteLine("hello mkpits");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student.display();
+            student s = new student();
+            s.display2();
+        }
+    }
+}
