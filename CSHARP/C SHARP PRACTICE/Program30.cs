b56d859bbb93fa40c47b04cd832e3c102ad14e37
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-18 16:26:11 +0530

    Csharp namespace example by using keyword

---
+++ CSHARP/C SHARP PRACTICE/Program30.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using first;
+using second;
+namespace first
+{
+    public class hello
+    {
+        public void sayhello() 
+        { 
+            Console.WriteLine("hello namespace");
+        }
+    }
+}
+namespace second
+{
+    public class welcome
+    {
+        public void saywelcome() 
+        { 
+            Console.WriteLine("welcome namespace"); 
+        }
+    }
+}
+public class TestNamespace
+{
+    public static void Main()
+    {
+        hello h1 = new hello();
+        welcome w1 = new welcome();
+        h1.sayhello();
+        w1.saywelcome();
+    }
+}
