bbeab457bc765e6dd8c3dcbe4597a5b19bc0cee2
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-08-18 15:16:31 +0530

    Csharp static class example

---
+++ CSHARP/C SHARP PRACTICE/Program26.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_static_class_example
+{
+    public static class school
+    {
+        public static float pi = 3.14f;
+        public static int cube(int n)
+        {
+            return n * n *n; 
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("value of pi:" + school.pi);
+            Console.WriteLine("cube of 3:" + school.cube(5));
+
+        }
+    }
+}
