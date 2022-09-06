a5668aad9140edfc01a137d5f5f5ed58f99666cf
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-06 18:16:29 +0530

    accept number and print their addition

---
+++ WINDOWS FORM/Accept_2no_print_addition_2no/Accept_2no_print_addition_2no/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_2no_print_addition_2no
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int Result = n1 + n2;
+            label3.Text = "addition of 2 no:" + Result;
+
+
+
+
+        }
+    }
+}
