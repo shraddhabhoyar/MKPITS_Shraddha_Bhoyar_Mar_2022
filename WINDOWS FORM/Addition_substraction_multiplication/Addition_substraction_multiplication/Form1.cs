5cbdf2c24ca2f4d7ff66e52b4e94cbf0da5bcc7f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-07 17:27:11 +0530

    perform addition,multiplication,substraction

---
+++ WINDOWS FORM/Addition_substraction_multiplication/Addition_substraction_multiplication/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Addition_substraction_multiplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int add = n1 + n2;
+            label3.Text = "Addition of 2 no :" + add;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int sub = n1 - n2;
+            label3.Text = "Substraction of 2 no :" + sub;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int mul = n1 * n2;
+            label3.Text = "Multiplication of 2 no :" + mul;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int div = n1 / n2;
+            label3.Text = "Divide of 2 no :" + div;
+        }
+    }
+}
