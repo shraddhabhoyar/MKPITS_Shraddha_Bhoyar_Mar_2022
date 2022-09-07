e2521d144eddd2da82372ba6a6479b40261dcbc9
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-07 17:39:44 +0530

    accept subject marks and display total,grade,percentage

---
+++ WINDOWS FORM/Accept_3_sub_marks_display_tot_per_gr/Accept_3_sub_marks_display_tot_per_gr/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_3_sub_marks_display_tot_per_gr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = Convert.ToInt32(textBox3.Text);
+            int total = a + b + c;
+            label4.Text = "total:" + total;
+            float per = (total / 300.0f) * 100.0f;
+            label5.Text = "percentage :" + per;
+            if (per >= 75)
+            {
+                label6.Text = "grade : Distinction";
+            }
+            else if (per >= 60 && per < 75)
+            {
+                label6.Text = "grade : first class";
+            }
+            else
+            {
+                label6.Text = "grade : fail";
+            }
+
+        }
+    }
+}
---
+++ WINDOWS FORM/Accept_3_sub_marks_display_tot_per_gr/Accept_3_sub_marks_display_tot_per_gr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_3_sub_marks_display_tot_per_gr
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
