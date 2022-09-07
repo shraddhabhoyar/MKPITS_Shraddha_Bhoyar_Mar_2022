46999c81a107c5644748553f99e8b915ac62776b
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-07 17:53:38 +0530

    accept no print one upto that number

---
+++ WINDOWS FORM/Accept_no_print_no_1_upto_that_no/Accept_no_print_no_1_upto_that_no/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_no_print_no_1_upto_that_no
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb= new StringBuilder();
+            int n = Convert.ToInt32(textBox1.Text);
+            for(int i=1;i<=n;i++)
+            {
+                sb.Append(i + "\n");
+            }
+            label2.Text= sb.ToString();
+        }
+    }
+}
