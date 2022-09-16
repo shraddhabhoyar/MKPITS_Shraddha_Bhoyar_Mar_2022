65982afa077a1718d3bc41bacf201c32201312b0
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-16 15:10:06 +0530

    accept number and print factorial using while loop

---
+++ WINDOWS FORM/ASSIGNMENT/ANprintfactusingwhileloop/Accept_no_print_factorial_using_while_loop/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_no_print_factorial_using_while_loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            int fact=1;
+
+            while(n>0)
+            {
+                fact = fact * n;
+                n--;
+            }
+            label2.Text = "factorial of number is :" + fact;
+        }
+    }
+}
---
+++ WINDOWS FORM/ASSIGNMENT/ANprintfactusingwhileloop/Accept_no_print_factorial_using_while_loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_no_print_factorial_using_while_loop
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
