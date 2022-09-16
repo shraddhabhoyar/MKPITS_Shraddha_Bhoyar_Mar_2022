91712fe6aa61343ba00d74f838631c4bde73c52e
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-16 14:56:56 +0530

    accept char and display vowels or not using switch condition

---
+++ WINDOWS FORM/chardisplayvowelsornotusingswitch/Accept_char_display_vowel_or_not_using_switch/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_char_display_vowel_or_not_using_switch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char ch = Convert.ToChar(textBox1.Text);
+
+            switch(ch)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+              
+                    label2.Text = "it is a vowel";
+                    break;
+
+                default:
+                    label2.Text = "it is not a vowel";
+                    break;
+            }
+        }
+    }
+}
