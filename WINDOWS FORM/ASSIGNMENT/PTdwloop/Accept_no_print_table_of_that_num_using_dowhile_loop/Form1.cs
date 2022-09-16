9d525ec09105e509f89da42c9d1e5307e1588cd1
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-16 15:29:56 +0530

    accept number and print table of that number using do while loop

---
+++ WINDOWS FORM/ASSIGNMENT/PTdwloop/Accept_no_print_table_of_that_num_using_dowhile_loop/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_no_print_table_of_that_num_using_dowhile_loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int n=Convert.ToInt32(textBox1.Text);
+            int result = 1;
+            int i = 1;
+            do
+            {
+                result = n * i;
+                
+                sb.Append(n + "*" + i + "=" + result + "\n");
+                i++;
+            }  while (i <= 10);
+            label2.Text = sb.ToString();
+        }
+    }
+}
---
+++ WINDOWS FORM/ASSIGNMENT/PTdwloop/Accept_no_print_table_of_that_num_using_dowhile_loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_no_print_table_of_that_num_using_dowhile_loop
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
