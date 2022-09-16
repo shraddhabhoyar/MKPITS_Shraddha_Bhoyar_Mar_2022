04163541c38815b7079749cf775536883f5b13e3
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-16 13:45:37 +0530

    accept 2no and display greater no

---
+++ WINDOWS FORM/Accept_2_no_display_greater_no/Accept_2_no_display_greater_no/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_2_no_display_greater_no
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            
+            if(n1>n2)
+            {
+                label3.Text = "Number1 is greater number";
+            }
+            else
+            {
+                label3.Text = "Number2 is greater number";
+            }
+
+        }
+    }
+}
