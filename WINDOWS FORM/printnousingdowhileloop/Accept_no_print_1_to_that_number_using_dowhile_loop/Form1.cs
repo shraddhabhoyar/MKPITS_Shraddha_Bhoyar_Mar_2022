f75178311b3eedbae5b8f9f09c8df4b575eff48d
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-16 15:03:49 +0530

    accept number and print 1 to that number using dowhile loop

---
+++ WINDOWS FORM/printnousingdowhileloop/Accept_no_print_1_to_that_number_using_dowhile_loop/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_no_print_1_to_that_number_using_dowhile_loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int n = Convert.ToInt32(textBox1.Text);
+            int i = 1;
+            do
+            {
+                sb.Append(i + "\n");
+                i++;
+            } while (i <= n);
+            label2.Text=sb.ToString();
+        }
+    }
+}
