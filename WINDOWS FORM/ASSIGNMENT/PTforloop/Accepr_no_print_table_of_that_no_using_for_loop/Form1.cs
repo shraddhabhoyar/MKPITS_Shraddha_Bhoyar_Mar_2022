eb960adef02ef402defda198c12b3688838e1c5a
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-16 15:27:17 +0530

    accept number and print table of that number using for loop

---
+++ WINDOWS FORM/ASSIGNMENT/PTforloop/Accepr_no_print_table_of_that_no_using_for_loop/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accepr_no_print_table_of_that_no_using_for_loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            StringBuilder sb = new StringBuilder();
+            int n=Convert.ToInt32(textBox1.Text);
+            int result = 1;
+
+            for(int i=1;i<=10;i++)
+            {
+                result = n * i;
+                sb.Append(n + "*" + i + "=" + result + "\n");
+                
+            }
+            label2.Text=sb.ToString();
+        }
+    }
+}
---
+++ WINDOWS FORM/ASSIGNMENT/PTforloop/Accepr_no_print_table_of_that_no_using_for_loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accepr_no_print_table_of_that_no_using_for_loop
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
