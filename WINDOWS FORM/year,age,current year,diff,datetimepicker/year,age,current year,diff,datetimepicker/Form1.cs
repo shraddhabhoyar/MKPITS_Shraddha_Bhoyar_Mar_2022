f4858b9ebefe46e2bd71befa29c64f373aee0464
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 20:22:33 +0530

    year,age,currentyear,difference and datetime picker

---
+++ WINDOWS FORM/year,age,current year,diff,datetimepicker/year,age,current year,diff,datetimepicker/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace year_age_current_year_diff_datetimepicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string age = dateTimePicker1.Text;
+            MessageBox.Show("dob  " + age);
+            string yr = age.Substring(5);
+            MessageBox.Show("dob year " + yr);
+            int cyr = DateTime.Now.Year;
+            MessageBox.Show("current year " + cyr);
+            int diff = cyr - Convert.ToInt32(yr);
+            MessageBox.Show("your age is " + diff);
+
+
+        }
+    }
+}
