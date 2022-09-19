c7476968e0d9e0b5eeb8c95b9a17781da0a5603f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 11:30:47 +0530

    new calculator function

---
+++ WINDOWS FORM/calculator_new/calculator_new/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator_new
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int prevnumber = 0;
+        string op = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+          if (textBox1.Text=="+" || textBox1.Text=="-")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="+" || textBox1.Text=="-")
+            {
+                textBox1.Text="2";
+
+            }
+            else
+            {
+                textBox1.Text = "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="+" || textBox1.Text== "-")
+            {
+                textBox1.Text="3";
+            }
+            else
+            {
+                textBox1.Text = "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            prevnumber = Convert.ToInt32(textBox1.Text);
+            op = "+";
+            textBox1.Text="+";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            prevnumber = Convert.ToInt32(textBox1.Text);
+            op="-";
+            textBox1.Text = "-";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int result=0;
+
+            switch(op)
+            {
+                case "+":
+                    result = Convert.ToInt32(textBox1.Text) + prevnumber;
+                    break;
+                    case "-":
+                    result = prevnumber - Convert.ToInt32(textBox1.Text);
+                    break ;
+
+            }
+            textBox1.Text=result.ToString  ();
+        }
+    }
+}
