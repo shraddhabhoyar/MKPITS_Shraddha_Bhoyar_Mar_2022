86f149543424716c4a9c0551db473a6c422c5b5b
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 11:07:44 +0530

    nested loop numbers programme

---
+++ WINDOWS FORM/nested_loop_numbers/nested_loop_numbers/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nested_loop_numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb=new StringBuilder();
+            int i, j;
+            for(i=1;i<=3;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    sb.Append(j + "  ");
+                }
+                sb.Append("\n");
+            }
+            label1.Text = sb.ToString();
+        }
+    }
+}
