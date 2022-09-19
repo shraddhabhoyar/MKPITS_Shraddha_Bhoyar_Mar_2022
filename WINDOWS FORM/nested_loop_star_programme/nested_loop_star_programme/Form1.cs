8d716b6d1efa9ca34e433c03b1a0598f6fd0b043
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 11:06:40 +0530

    nested loop star programme

---
+++ WINDOWS FORM/nested_loop_star_programme/nested_loop_star_programme/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nested_loop_star_programme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb=new StringBuilder();
+            int i, j;
+            for(i=1;i<=10; i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    sb.Append("*" + "  ");
+
+                }
+                sb.Append("\n");
+            }
+            label1.Text= sb.ToString(); 
+        }
+    }
+}
