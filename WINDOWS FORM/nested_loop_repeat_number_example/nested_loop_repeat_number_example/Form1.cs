f27928c166e6e3f8ca9dbd1b6bb6a25dfc59ff39
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 11:09:52 +0530

    nested loop repeat number programme

---
+++ WINDOWS FORM/nested_loop_repeat_number_example/nested_loop_repeat_number_example/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nested_loop_repeat_number_example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int i, j;
+            for (i = 1; i <= 7; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    sb.Append(i + "  ");
+                }
+                sb.Append("\n");
+            }
+            label1.Text = sb.ToString();
+
+        }
+    }
+}
