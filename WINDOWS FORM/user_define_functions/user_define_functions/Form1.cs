3f15b7f0152b4169542f3d1f451473d0a802edae
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 11:25:36 +0530

    user define functions

---
+++ WINDOWS FORM/user_define_functions/user_define_functions/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace user_define_functions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] album = new string[2, 2];
+        StringBuilder sb = new StringBuilder();
+        public void display()
+        {
+            album[0, 0] = textBox1.Text;
+            album[0, 1] = textBox2.Text;
+            album[1, 0] = textBox3.Text;
+            album[1, 1] = textBox4.Text;
+            sb.Append("album name " + album[0, 0] + " : singer name " + album[0, 1] + "\n");
+            sb.Append("album name " + album[1, 0] + " : singer name " + album[1, 1] + "\n");
+            label5.Text = sb.ToString();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            display();
+        }
+    }
+}
