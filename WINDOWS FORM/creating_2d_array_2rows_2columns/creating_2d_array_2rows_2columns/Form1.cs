a00b305b18cc23b310880c52be3e554f6c649f54
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 11:22:27 +0530

    creating 2d array with 2 rows and 2 columns

---
+++ WINDOWS FORM/creating_2d_array_2rows_2columns/creating_2d_array_2rows_2columns/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace creating_2d_array_2rows_2columns
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] album = new string[2, 2];
+        StringBuilder sb=new StringBuilder();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            album[0, 0] = textBox1.Text;
+            album[0, 1] = textBox2.Text;
+            album[1, 0] = textBox3.Text;
+            album[1, 1] = textBox4.Text;
+            sb.Append("album name" + album[0, 0] + ", singer name" + album[0, 1] + "\n");
+            sb.Append("album name" + album[1, 0] + ", singer name" + album[1, 1] + "\n");
+            label5.Text=sb.ToString();
+        }
+    }
+}
