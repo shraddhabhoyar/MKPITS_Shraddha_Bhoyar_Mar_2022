9861bfde587d1766199819bd9cd3305755e1f81a
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 11:32:52 +0530

    windows form using radiobutton

---
+++ WINDOWS FORM/windows_forms_using_radiobutton/windows_forms_using_radiobutton/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_forms_using_radiobutton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int bal = 1000;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int accno = Convert.ToInt32(textBox1.Text);
+            int amt = Convert.ToInt32(textBox2.Text);
+
+            if(radioButton1.Checked)
+            {
+                bal = bal + amt;
+            }
+            else if(radioButton2.Checked)
+            {
+                bal =bal- amt;
+            }
+            label3.Text = "bal is" + bal;
+        }
+    }
+}
