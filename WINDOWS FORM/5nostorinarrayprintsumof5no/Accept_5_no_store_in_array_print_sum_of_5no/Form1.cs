3e2a57f7afe9009aba301d4a1414a6b15dd6509a
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-19 11:12:36 +0530

    accept 5 numbers store in array and print sum of 5numbers

---
+++ WINDOWS FORM/5nostorinarrayprintsumof5no/Accept_5_no_store_in_array_print_sum_of_5no/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_5_no_store_in_array_print_sum_of_5no
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        int[] num = new int[5];
+        int i = 0;
+        int sum = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i < 5)
+            {
+                num[i] = Convert.ToInt32(textBox1.Text);
+                sum = sum + num[i];
+                i++;
+                textBox1.Clear();
+                textBox1.Focus();
+                if (i == 5)
+                {
+                    label2.Text = "sum of 5 no." + sum;
+                }
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+}
---
+++ WINDOWS FORM/5nostorinarrayprintsumof5no/Accept_5_no_store_in_array_print_sum_of_5no/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accept_5_no_store_in_array_print_sum_of_5no
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
