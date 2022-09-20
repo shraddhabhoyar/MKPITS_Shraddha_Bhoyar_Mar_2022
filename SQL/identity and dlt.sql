08e2905c4e4da09e398ce29cbd539a836d21dc0f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 18:19:53 +0530

    identity and delete

---
+++ SQL/identity and dlt.sql
@@ -0,0 +1,11 @@
+create database emp
+create table employee(empid int primary key identity(1,1),empname varchar(20))
+insert into employee (empname) values('rajnish')
+select*from employee
+delete from employee
+insert into employee(empname)values('ashish')
+select*from employee
+insert into employee(empname) values('rohan')
+select*from employee
+insert into employee(empname) values('akshay')
+select*from employee
