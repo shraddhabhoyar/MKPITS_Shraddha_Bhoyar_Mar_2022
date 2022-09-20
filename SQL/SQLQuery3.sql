769ff509de3bdeb81aeff4f3fd65cb206608dd39
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 18:18:21 +0530

    third sample

---
+++ SQL/SQLQuery3.sql
@@ -0,0 +1,14 @@
+create database mkp
+create table customer (custid int primary key,custname varchar(20))
+insert into customer values(123,'ankita')
+select*from customer
+insert into customer values(1,'ankita')
+select*from customer
+
+
+
+
+
+
+
+
