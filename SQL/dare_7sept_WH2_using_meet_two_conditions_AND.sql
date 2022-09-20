43c275688bc85451d9c24842a487e4838a8198c0
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 20:59:45 +0530

    where using meet two condition AND

---
+++ SQL/dare_7sept_WH2_using_meet_two_conditions_AND.sql
@@ -0,0 +1,14 @@
+create table student1(cid int,cname varchar(20),cadd varchar(40))
+insert into student1 values(112,'vipin','nandanvan')
+insert into student1 values(113,'kumar','wadi')
+insert into student1 values(114,'shrwan','shantinagar')
+insert into student1 values(115,'lucky','bharat nagar')
+insert into student1 values(116,'ganesha','manish nagar')
+insert into student1 values(117,'khushal','besa')
+insert into student1 values(118,'shakib','beltarodi')
+insert into student1 values(119,'rahim','saoner')
+insert into student1 values(120,'pintukumar','itwari')
+insert into student1 values(121,'kiran','mahal')
+select * from student1 where cname='vipin' and cadd='nandanvan' 
+
+
