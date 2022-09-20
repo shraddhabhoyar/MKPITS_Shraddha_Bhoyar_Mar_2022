de661681887615de57393b9788f0c46b84192975
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 21:29:14 +0530

    first normal form

---
+++ SQL/date_12sept_1NF.sql
@@ -0,0 +1,5 @@
+create table employee(emp_id int,emp_name varchar(20),emp_phone varchar(10),emp_state varchar(10))
+insert into employee values(1,'anita','9876543210','mp')
+insert into employee values(2,'amit','9876543211','mah')
+
+select * from employee
