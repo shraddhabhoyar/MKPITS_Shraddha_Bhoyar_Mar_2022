7c4ca6ca3baa75fff9399998803fa6dacc409269
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 21:31:58 +0530

    inner join

---
+++ SQL/date12sept_INNERJOIN.sql
@@ -0,0 +1,28 @@
+create table students(id int primary key identity,admission_no varchar(30) not null,
+first_name varchar(30)not null,last_name varchar(30) not null,age int,city varchar(30)not null)
+
+create table fee(admission_no varchar(30) not null,course varchar(30) not null,amount_paid int)
+
+insert into students (admission_no,first_name,last_name,age,city)
+values(3354,'Luisa', 'Evans', 13, 'Texas'),
+       (2135, 'Paul', 'Ward', 15, 'Alaska'),       
+    (4321, 'Peter', 'Bennett', 14, 'California'),    
+    (4213,'Carlos', 'Patterson', 17, 'New York'),       
+    (5112, 'Rose', 'Huges', 16, 'Florida'),  
+    (6113, 'Marielia', 'Simmons', 15, 'Arizona'),    
+    (7555,'Antonio', 'Butler', 14, 'New York'),       
+    (8345, 'Diego', 'Cox', 13, 'California')
+
+	insert into fee(admission_no,course,amount_paid)
+	values(3354,'Java', 20000),       
+    (7555, 'Android', 22000),       
+    (4321, 'Python', 18000),    
+    (8345,'SQL', 15000),       
+    (5112, 'Machine Learning', 30000)
+
+	select * from students
+	select * from fee
+
+
+
+
