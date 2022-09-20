a7b2f98bfff598220e59264b68f16475c01ff41a
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 20:36:56 +0530

    accept and show customer id,name and address

---
+++ SQL/ASSIGNMENT/assignment no2.sql
@@ -0,0 +1,24 @@
+create database mk
+create table cu(cid int,cname varchar(20),cadd varchar(40))
+insert into cu values(112,'vipin','nandanvan')
+select*from cu
+insert into cu values(113,'kumar','wadi')
+select*from cu
+insert into cu values(114,'shrwan','shantinagar')
+select*from cu
+insert into cu values(115,'lucky','bharat nagar')
+select*from cu
+insert into cu values(116,'ganesha','manish nagar')
+select*from cu
+insert into cu values(117,'khushal','besa')
+select*from cu
+insert into cu values(118,'shakib','beltarodi')
+select*from cu
+insert into cu values(119,'rahim','saoner')
+select*from cu
+insert into cu values(120,'pintukumar','itwari')
+select*from cu
+insert into cu values(121,'kiran','mahal')
+select*from cu
+
+
