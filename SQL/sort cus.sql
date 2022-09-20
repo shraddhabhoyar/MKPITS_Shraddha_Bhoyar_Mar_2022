c877964422642029bf85580236e3df67828492de
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 20:40:45 +0530

    sort method

---
+++ SQL/sort cus.sql
@@ -0,0 +1,16 @@
+create database cus
+
+create table cus(cid int,cname varchar(20),cadd varchar(40))
+insert into cus values(112,'vipin','nandanvan')
+insert into cus values(113,'kumar','wadi')
+insert into cus values(114,'shrwan','shantinagar')
+insert into cus values(115,'lucky','bharat nagar')
+insert into cus values(116,'ganesha','manish nagar')
+insert into cus values(117,'khushal','besa')
+insert into cus values(118,'shakib','beltarodi')
+insert into cus values(119,'rahim','saoner')
+insert into cus values(120,'pintukumar','itwari')
+insert into cus values(121,'kiran','mahal')
+select * from cus order by cname
+
+
