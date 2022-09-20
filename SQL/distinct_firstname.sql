4c9d6654f3c7b1dda7699248245b804471146fdd
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 20:53:50 +0530

    distinct

---
+++ SQL/distinct_firstname.sql
@@ -0,0 +1,6 @@
+create table fi(firstname varchar(20),lastname varchar(20),coursename varchar(20), city varchar(20),sta varchar(20))
+insert into fi values('rohit','kumar','dotnet','nagpur','mah')
+insert into fi values('laxmi','rai','dotnet','nagpur','mah')
+insert into fi values('chetan','shastri','dotnet','mumbai','mah')
+insert into fi values('hema','naidu','java','nagpur','mah')
+select distinct firstname from fi
