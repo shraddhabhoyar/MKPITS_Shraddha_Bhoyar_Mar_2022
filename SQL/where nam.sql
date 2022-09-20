6541df65d5bc3c91615c178dc0678df854199500
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 20:42:45 +0530

    where

---
+++ SQL/where nam.sql
@@ -0,0 +1,13 @@
+create database nam
+create table nam(cid int,cname varchar(20),cadd varchar(40))
+insert into nam values(112,'vipin','nandanvan')
+insert into nam values(113,'kumar','wadi')
+insert into nam values(114,'shrwan','shantinagar')
+insert into nam values(115,'lucky','bharat nagar')
+insert into nam values(116,'ganesha','manish nagar')
+insert into nam values(117,'khushal','besa')
+insert into nam values(118,'shakib','beltarodi')
+insert into nam values(119,'rahim','saoner')
+insert into nam values(120,'pintukumar','itwari')
+insert into nam values(121,'kiran','mahal')
+select*from nam where cname='shakib'
