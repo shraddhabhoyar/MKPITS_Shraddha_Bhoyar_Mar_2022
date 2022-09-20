8bb5e2e5d47e4ad089be230946c9b46fb3d7734d
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 18:05:36 +0530

    creating and using classes in windowsform using runtime polymorphyism

---
+++ WINDOWS FORM/classeswfruntimepoly/creating_using_classes_wf_runtime_polymorphism/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace creating_using_classes_wf_runtime_polymorphism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        account act = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            act.accno = Convert.ToInt32(textBox1.Text);
+            string str = act.deposite(Convert.ToInt32(textBox2.Text));
+            label4.Text = str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            act.accno = Convert.ToInt32(textBox1.Text);
+            string str = act.withdrawl(Convert.ToInt32(textBox2.Text));
+            label4.Text = str;
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            act.accno = Convert.ToInt32(textBox1.Text);
+            string str = act.withdrawl(Convert.ToInt32(textBox2.Text));
+            label4.Text = "account no : " + act.accno + " : " + str;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "saving")
+            {
+                act = new saving();
+            }
+            else if (comboBox1.Text == "current")
+            {
+                act = new current();
+            }
+
+        }
+    }
+}
---
+++ WINDOWS FORM/classeswfruntimepoly/creating_using_classes_wf_runtime_polymorphism/call.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creating_using_classes_wf_runtime_polymorphism
+{
+    abstract class account
+    {
+        public int accno;
+        public int bal = 1000;
+        public abstract string deposite(int amt);
+
+        public string withdrawl(int amt)
+        {
+            string res = null;
+
+            if (amt > bal)
+            {
+                res = "insufficient balance";
+            }
+            else
+            {
+                bal = bal - amt;
+                res = "withdrawl successfully " + bal.ToString();
+            }
+            return res;
+        }
+        public string showbalance()
+        {
+            return "balance amount is " + bal.ToString();
+        }
+
+    }
+    class saving :account
+    {
+        public override string deposite(int amt)
+        {
+            int interest = 500;
+            bal = bal + amt + interest;
+            return "amount deposited successfully with interest , bal is " + bal.ToString();
+
+        }
+    }
+    class current : account
+    {
+        public override string deposite(int amt)
+        {
+
+            bal = bal + amt;
+            return "amount deposited successfully without interest , bal is " + bal.ToString();
+
+        }
+    }
+}
+
+    
+
+
+
+        
+
