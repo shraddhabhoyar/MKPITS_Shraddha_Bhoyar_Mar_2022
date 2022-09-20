3e3fe222956e647aee41cffe5c6972dd4591c67f
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 20:34:39 +0530

    accept and show rno,name,address,mobilenumber

---
+++ SQL/ASSIGNMENT/assignment no1.sql
@@ -0,0 +1,31 @@
+create database mkpits3
+
+create table st(rno int,nam varchar(20),ads varchar(40),mob float )
+insert into st values(301,'abhay','nagpur',9087654321)
+select*from st
+insert into st values(302,'abha','pune',8765438999)
+select*from st
+insert into st values(303,'bharti','hydrabad',7865436789)
+select*from st
+insert into st values(304,'bhagyshree','delhi',7865098745)
+select*from st
+insert into st values(305,'chitra','kolkata',90876545898)
+select*from st
+insert into st values(306,'dhiraj','nagpur',9045379023)
+select*from st
+insert into st values(307,'ganesh','banglore',8888888883)
+select*from st
+insert into st values(308,'harish','nagpur',90045673865)
+select*from st
+insert into st values(309,'riya','delhi',9087654321)
+select*from st
+insert into st values(310,'priya','nagpur',7789056436)
+select*from st
+
+
+
+
+
+
+
+
