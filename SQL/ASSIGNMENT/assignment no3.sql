8a8a88eb7ac82bf5ffd59127473d759a157c83a7
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 20:38:47 +0530

    accept and show user id,name,password,mailid,gender,city,country

---
+++ SQL/ASSIGNMENT/assignment no3.sql
@@ -0,0 +1,24 @@
+create database us
+create table users(userid int,username varchar(20),userpass varchar(20),emailid varchar(20),gender varchar(20),city varchar(20),country varchar(20))
+insert into users values(1,'ankita','aaaa','ankita@gmail.com','female','nagpur','india')
+select*from users
+insert into users values(2,'aniket','bbbb','aniket1@gmail.com','male','nagpur','india')
+select*from users
+insert into users values(3,'reva','cccc','reva123@gmail.com','female','nagpur','india')
+select*from users
+insert into users values(4,'kirti','dddd','kirti675@gmail.com','female','nagpur','india')
+select*from users
+insert into users values(5,'akshay','brtbb','akshay21@gmail.com','male','nagpur','india')
+select*from users
+insert into users values(6,'bhushan','retb','bhushan23@gmail.com','male','nagpur','india')
+select*from users
+insert into users values(7,'ankur','eeee','ankur341@gmail.com','male','nagpur','india')
+select*from users
+insert into users values(8,'lax','gggg','laxb123@gmail.com','male','nagpur','india')
+select*from users
+insert into users values(9,'ravina','xyzz','ravina1234@gmail.com','female','nagpur','india')
+select*from users
+insert into users values(10,'sanjivani','ssss','sanju678@gmail.com','female','nagpur','india')
+select*from users
+
+
