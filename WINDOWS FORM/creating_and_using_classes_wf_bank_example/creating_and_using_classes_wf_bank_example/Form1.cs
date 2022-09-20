71bd9cfb2765c119fdb62711bbce7c97d1bd7e6a
shraddhabhoyar <shraddhabhoyar639@gmail.com>
2022-09-20 17:51:40 +0530

    creating and using classes in windows form bank example

---
+++ WINDOWS FORM/creating_and_using_classes_wf_bank_example/creating_and_using_classes_wf_bank_example/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace creating_and_using_classes_wf_bank_example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bank b=new bank();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b.accno = Convert.ToInt32(textBox1.Text);
+            string str = b.deposit(Convert.ToInt32(textBox2.Text));
+            label3.Text= str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            b.accno = Convert.ToInt32(textBox1.Text);
+            string str = b.withdrawl(Convert.ToInt32(textBox2.Text));
+            label3.Text=str;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            b.accno = Convert.ToInt32(textBox1.Text);
+            string str = b.showbalance();
+            label3.Text = "accno :" + b.accno + ":" + str;
+        }
+    }
+}
---
+++ WINDOWS FORM/creating_and_using_classes_wf_bank_example/creating_and_using_classes_wf_bank_example/bank.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creating_and_using_classes_wf_bank_example
+{
+    internal class bank
+    {
+        public int accno;
+        public int bal = 1000;
+
+        public string deposit (int amount)
+        {
+            bal = bal + amount;
+            return "amount deposited successfully,bal is" + bal.ToString();
+        }
+        public string withdrawl (int amount)
+        {
+            string res = null;
+            if(amount>bal)
+            {
+                res = "insufficient balance";
+            }
+            else
+            {
+                bal = bal - amount;
+                res = "amount withdrawl successfully,bal is" + bal.ToString();
+            }
+            return res;
+        }
+        public string showbalance()
+        {
+            return "balance amount is" + bal.ToString();
+        }
+    }
+}
